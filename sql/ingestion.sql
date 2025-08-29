98a8fda00563b1270a8fd96bf508850b76d4f23d
Mahesh M <160126376+XTR2k4@users.noreply.github.com>
2025-08-29 12:53:59 +0530

    Update ingestion.sql

--- sql/ingestion.sql
+++ sql/ingestion.sql
@@ -1,3 +1,4 @@
+-- Loading all the reviews about various businesses
 CREATE OR REPLACE TABLE yelp_reviews (review_text VARIANT)
 
 COPY INTO yelp_reviews
@@ -8,26 +9,14 @@ CREDENTIALS = (
 )
 FILE_FORMAT = (TYPE = JSON);
 
---creating structured table 
-CREATE OR REPLACE TABLE tbl_reviews AS
-SELECT review_text:business_id::STRING AS business_id,
-    review_text:user_id::STRING AS user_id,
-    review_text:date::DATE AS review_date,
-    review_text:stars::NUMBER AS review_stars,
-    review_text:text::STRING AS review_txt,
-    analyze_sentiment(review_txt) AS sentiment
-FROM yelp_reviews;
 
-
+-- Loading data about the various businesses 
 CREATE OR REPLACE TABLE yelp_businesses (business_text VARIANT)
-  
---creating structured table for yelp_businesses
-CREATE OR REPLACE TABLE tbl_businesses AS
-SELECT business_text:business_id::STRING AS business_id,
-    business_text:name::STRING AS business_name,
-    business_text:city::STRING AS city,
-    business_text:state::STRING AS state,
-    business_text:review_count::NUMBER AS review_count,
-    business_text:stars::NUMBER AS stars,
-    business_text:categories::STRING AS categories
-FROM yelp_businesses;
+
+COPY INTO yelp_businesses
+FROM 's3://yelp-reviews-xt/business'
+CREDENTIALS = (
+    AWS_KEY_ID = '*******************'
+    AWS_SECRET_KEY = 'your_key'
+)
+FILE_FORMAT = (TYPE = JSON);
